d2ad16c0f063530455f010445ba6eb70345c27ac
Brayden Zee <github@blueish.dev>
2025-01-10 15:41:52 -0500

    Add smoothing to the current detection in the coral grabber

--- src/main/java/frc/robot/config/CoralGrabberConfig.java
+++ src/main/java/frc/robot/config/CoralGrabberConfig.java
@@ -4,9 +4,12 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class CoralGrabberConfig {
   public static final IdleMode IDLE_MODE = IdleMode.kBrake;
-  public static final int CURRENT_LIMIT = 20;
+  public static final int CURRENT_LIMIT = 20; // amps
 
   public static final double RELEASE_SPEED = 0.5;
   public static final double RELEASE_TIME = 0.5; // seconds
   public static final double GRAB_SPEED = 0.5;
+
+  public static final double FILTER_TIME_CONSTANT = 0.1;
+  public static final double TRIGGER_CURRENT = 19; // amps
 }
--- src/main/java/frc/robot/subsystems/CoralGrabber.java
+++ src/main/java/frc/robot/subsystems/CoralGrabber.java
@@ -5,6 +5,9 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,6 +16,9 @@ import frc.robot.config.CoralGrabberConfig;
 
 public class CoralGrabber extends SubsystemBase {
   private SparkMax motor = new SparkMax(CANMappings.END_EFFECTOR_ID, MotorType.kBrushless);
+  private LinearFilter filter =
+      LinearFilter.singlePoleIIR(
+          CoralGrabberConfig.FILTER_TIME_CONSTANT, TimedRobot.kDefaultPeriod);
 
   public CoralGrabber() {
     // Running positive should grab the coral, negative should release it
@@ -24,10 +30,16 @@ public class CoralGrabber extends SubsystemBase {
         PersistMode.kPersistParameters);
   }
 
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("coralGrabber/current", filter.lastValue());
+  }
+
   public Command grabCoral() {
     return Commands.runEnd(
             () -> motor.set(CoralGrabberConfig.GRAB_SPEED), () -> motor.stopMotor(), this)
-        .until(() -> motor.getOutputCurrent() > 19);
+        .until(
+            () -> filter.calculate(motor.getOutputCurrent()) > CoralGrabberConfig.TRIGGER_CURRENT);
   }
 
   public Command releaseCoral() {
