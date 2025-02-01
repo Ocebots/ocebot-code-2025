6c7fe380ef511fad0d5cb71d9b10151b570b1637
Brayden Zee <github@blueish.dev>
2025-02-01 16:07:56 -0500

    drivetrain: update can ids

--- src/main/java/frc/robot/config/CANMappings.java
+++ src/main/java/frc/robot/config/CANMappings.java
@@ -1,14 +1,14 @@
 package frc.robot.config;
 
 public class CANMappings {
-  public static final int FRONT_LEFT_DRIVING = 3;
-  public static final int REAR_LEFT_DRIVING = 5;
-  public static final int FRONT_RIGHT_DRIVING = 1;
-  public static final int REAR_RIGHT_DRIVING = 7;
-  public static final int FRONT_LEFT_TURNING = 4;
-  public static final int REAR_LEFT_TURNING = 6;
-  public static final int FRONT_RIGHT_TURNING = 2;
-  public static final int REAR_RIGHT_TURNING = 8;
+  public static final int FRONT_LEFT_DRIVING = 1;
+  public static final int REAR_LEFT_DRIVING = 7;
+  public static final int FRONT_RIGHT_DRIVING = 3;
+  public static final int REAR_RIGHT_DRIVING = 5;
+  public static final int FRONT_LEFT_TURNING = 2;
+  public static final int REAR_LEFT_TURNING = 8;
+  public static final int FRONT_RIGHT_TURNING = 4;
+  public static final int REAR_RIGHT_TURNING = 6;
 
   public static final int END_EFFECTOR_ID = 0;
   public static final int ALGAE_WHEEL_ID = 0;
