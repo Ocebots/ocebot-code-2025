8bff40ed18770dd7f4b3bb609ef62cf77bee8384
Brayden Zee <github@blueish.dev>
2025-02-04 15:08:12 -0500

    Fix precommit script

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
-echo "#!/usr/bin/env zsh\n\n./gradlew spotlessApply" > .git/hooks/pre-commit
+echo "#!/usr/bin/env zsh\n\n./gradlew build\n./gradlew spotlessCheck" > .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
