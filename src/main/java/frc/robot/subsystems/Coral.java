37a258783b39466aec0c6f4b16c84a048b9a7940
liamq16 <156020186+liamq16@users.noreply.github.com>
2025-02-06 15:51:36 -0500

    Added Coral subsystem

---
+++ src/main/java/frc/robot/subsystems/Coral.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.EncoderConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Coral extends SubsystemBase {
+    private CoralGrabber grabber = new CoralGrabber();
+    private Elevator elevator = new Elevator();
+    private CoralPivot coralPivot = new CoralPivot();
+}
