70203b7d6be65a1d1ec9b88af70c79eef3e4b53a
yay <80125491+Yaypixel@users.noreply.github.com>
2025-02-14 16:05:12 -0500

    coral movement: go to coral

--- src/main/java/frc/robot/config/CoralGrabberConfig.java
+++ src/main/java/frc/robot/config/CoralGrabberConfig.java
@@ -6,6 +6,8 @@ public class CoralGrabberConfig {
   public static final IdleMode IDLE_MODE = IdleMode.kBrake;
   public static final int CURRENT_LIMIT = 20; // amps
 
+
+
   public static final double RELEASE_SPEED = 0.5;
   public static final double RELEASE_TIME = 0.5; // seconds
   public static final double GRAB_SPEED = 0.5;
