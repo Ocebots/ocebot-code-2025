42c9be5589ba061876124963bfc4f6a33f3087e6
Brayden Zee <github@blueish.dev>
2025-02-14 15:23:01 -0500

    add getters for positions

---
+++ src/main/java/frc/robot/config/Positions.java
@@ -0,0 +1,36 @@
+package frc.robot.config;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public class Positions {
+  public static Pose2d getReef() {
+    return new Pose2d(
+        Units.inchesToMeters(
+            DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Blue
+                ? 176.75
+                : 460.5866666666 - 176.75),
+        Units.inchesToMeters(158.50),
+        new Rotation2d(0));
+  }
+
+  public static Pose2d getIndividualReef(int idx) {
+    return getReef()
+        .transformBy(
+            new Transform2d(
+                new Translation2d(0.801751, Units.inchesToMeters(12.94) / 2.0 * (idx % 2 * 2 - 1))
+                    .rotateBy(
+                        Rotation2d.fromDegrees(
+                            60.0 * (double) (idx / 2)
+                                + (DriverStation.getAlliance().orElse(Alliance.Blue)
+                                        == Alliance.Blue
+                                    ? 0.0
+                                    : 180.0))),
+                new Rotation2d()));
+  }
+}
