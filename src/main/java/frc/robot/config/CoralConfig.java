3a409fe94545c9c8e37e4607a020a8d9df17d875
yay <80125491+Yaypixel@users.noreply.github.com>
2025-02-14 16:07:44 -0500

    coral movement: fixes

--- src/main/java/frc/robot/config/CoralConfig.java
+++ src/main/java/frc/robot/config/CoralConfig.java
@@ -1,9 +1,9 @@
 package frc.robot.config;
 
 public class CoralConfig {
-    public static final double MOVEMENT_P = 0.0;
-    public static final double MOVEMENT_I = 0.0;
-    public static final double MOVEMENT_D = 0.0;
+  public static final double MOVEMENT_P = 0.0;
+  public static final double MOVEMENT_I = 0.0;
+  public static final double MOVEMENT_D = 0.0;
 
-    public static final double MOVEMENT_DISTANCE = 2;
+  public static final double MOVEMENT_DISTANCE = 2;
 }
