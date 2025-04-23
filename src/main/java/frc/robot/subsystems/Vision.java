e9e770dc9ec092cd232e2a4f509cf9f58f7f1bfe
KaraParikh1 <karaaparikh@gmail.com>
2025-04-23 16:23:08 -0400

    commented Vision

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -5,5 +5,7 @@ import org.photonvision.PhotonCamera;
 
 public class Vision {
   public static final PhotonCamera camera = new PhotonCamera(VisionConfig.CAMERA_NAME);
+  // Creates a new camera with the designated name from the config
   public static final PhotonCamera secondCamera = new PhotonCamera(VisionConfig.SECOND_CAMERA_NAME);
+  // Creates a new camera with the designated name from the config
 }
